3461732aac24094d5628954d4587207c3f0481f6
Yair Castro <106456565+joseycastror@users.noreply.github.com>
2026-02-08 17:47:09 -0600

    Delete challenge-forohub/forohub/target directory

--- challenge-forohub/forohub/target/classes/db/migration/V1__create-table-usuarios.sql
+++
@@ -1,6 +0,0 @@
-create table usuarios (
-    id bigint not null auto_increment primary key,
-    nombre varchar(255) not null,
-    email varchar(100) not null unique,
-    passwd varchar(255) not null
-);
